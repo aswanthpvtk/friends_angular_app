0b4cb60aa7393776f7f3c18955bc943e08cfca35
Aswanth P <Aswanth@NTP-LAP-734.chn.nestgroup.net>
2022-11-28 22:11:44 +0530

    read

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent {
+name=""
+
+readValues=()=>
+{
+  let data:any={"name":this.name}
+  console.log(data)
+  
+}
 
 }
