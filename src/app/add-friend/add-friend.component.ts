7d71f4fb5fac98a76370e60b7304c499514f4e8f
Aswanth P <Aswanth@NTP-LAP-734.chn.nestgroup.net>
2022-11-30 10:22:36 +0530

    apiadd

--- src/app/add-friend/add-friend.component.ts
+++ src/app/add-friend/add-friend.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-add-friend',
@@ -10,6 +11,7 @@ export class AddFriendComponent {
   friendName=""
   friendNickName=""
   DescribeYourFriend=""
+  constructor(private api:ApiService){}
 
 
   readValues=()=>
@@ -19,6 +21,35 @@ export class AddFriendComponent {
  let data:any={"name":this.name,"friendName":this.friendName,"friendNickName":this.friendNickName,"DescribeYourFriend":this.DescribeYourFriend}
 
 console.log(data)
+
+this.api.addfriend(data).subscribe(
+  (response:any)=>
+  {
+    console.log(response)
+    if (response.status == "success") {
+
+      alert("Added friend successfully")
+
+      this.name=""
+
+      this.friendName=""
+
+      this.DescribeYourFriend=""
+
+      this.friendNickName=""
+
+      
+
+    } else {
+
+      alert("Something went wrong")
+
+    }
+  
+  
+  }
+)
+
 
 }
 
